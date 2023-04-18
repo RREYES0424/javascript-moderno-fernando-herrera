660f6a26a3cf02f9d44cad6c7c17fa3cb96d254b
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-18 13:55:37 -0600

    ADDED: palabras-reservadas.js - Palabras reservadas y como nombrar las variables correctamente

---
+++ 01-FUNDAMENTOS/assets/js/palabras-reservadas.js
@@ -0,0 +1,16 @@
+// const
+// if
+// try
+// while
+// for()
+// enum
+let objetos$123;
+let preci099_99;
+let xPos;
+
+
+// Los nombres de las variables pueden empezar con letras, o los símbolos $ o _. Solo pueden contener letras, números, $ y _.  No pueden empezar con un número. "myVariable", "leaf_1" y "$money3" son todos ejemplos válidos de nombres de variables.
+// Los nombres de las variables distinguen entre mayúsculas y minúsculas, lo que significa que "xPos" es diferente de "xpos", así que asegúrate de ser consistente.
+// Los nombres de las variables no pueden ser los mismos que los nombres de las variables existentes, y hay muchas en nuestro entorno de programación de ProcessingJS. Si alguna vez te sale un error como "Read only!", trata de cambiar el nombre de tu variable.
+// Los nombres de las variables deben ser claros y con sentido; por ejemplo, en vez de "ts", usa "toothSize".
+// Los nombres de las variables deben usar el formato mayúscula y minúscula (camelCase) para varias palabras, como "toothSize" en vez de "toothsize" o "tooth_size".
