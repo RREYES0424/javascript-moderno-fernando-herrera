306986d5e827e6fa7805fbf21238516efd4053ab
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-18 12:19:15 -0600

    MODIFIED: app.js - Introduccion a la consola

--- 01-FUNDAMENTOS/app.js
+++ 01-FUNDAMENTOS/app.js
@@ -1,6 +1,17 @@
 console.log('Hola Mundo!');
 
-let a = 10, b = 10, c = 10, d = a + b + c ;
+//Introduccion a variables
+let a = 10, b = 10, c = 10, d = a + b + c, hero = 'Spiderman', hi = 'Hola ';
 var x = 10;
-const z = 10;
+const z = 10, saludo = hi + hero;
 
+//Tipos de console.log
+// console.log('Console Log normal');
+// console.warn('Mensaje de Warning');
+// console.error('Mensaje de Error');
+// console.info('Mensaje Informativo');
+
+// console.log('%c Mis Variables', 'color:hotpink; font-weight:bold');
+// console.log('a =',a);
+// console.log({a},{b},{c});
+// console.table({a, b, c, d, x, z, hero, saludo});
