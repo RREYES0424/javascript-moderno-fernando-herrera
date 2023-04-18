441d0d4d4543132928d950abe5b13d5474fab39e
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-18 13:36:22 -0600

    ADDED: primitivos.js - Conociendo los datos primitivos

---
+++ 01-FUNDAMENTOS/assets/js/primitivos.js
@@ -0,0 +1,18 @@
+let nombre = "Ricardo Reyes Flores";
+nombre = "Otro nombre";
+console.log(typeof nombre);
+
+let isActive = true;
+console.log(typeof isActive);
+
+let edad = 33;
+console.log(typeof edad);
+
+let superPoderSpiderMan;
+console.log(typeof superPoderSpiderMan);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+console.log(typeof symbol1);
