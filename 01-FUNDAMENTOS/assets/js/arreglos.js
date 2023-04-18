e8dfec2990cf9b16f89090713fcc78239ffa687c
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-18 14:40:21 -0600

    ADDED: arreglos.js - Introduccion a los arreglos

---
+++ 01-FUNDAMENTOS/assets/js/arreglos.js
@@ -0,0 +1,24 @@
+// const arr = new Array(10);
+// console.log(arr);
+// const arr = [];
+
+let videojuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+console.log({videojuegos});
+console.log(videojuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Ricardo',
+    1 + 2,
+    otroArray = [true, 123, 'Ricardo'],
+    function(){},
+    ()=>{},
+    {a:1},
+    ['X', 'Megaman', 'Zero', 'Dr.Light',[
+        'Dr. Willy',
+        'Woodman',
+    ]],
+];
+
+console.log(arregloCosas[8][4][1]);
