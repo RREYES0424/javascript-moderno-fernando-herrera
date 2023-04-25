ca3b6123da95cd907dd0cab50ac405626d5ef2d3
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-25 13:42:49 -0600

    ADDED: switch.js

---
+++ 01-FUNDAMENTOS/assets/js/switch.js
@@ -0,0 +1,28 @@
+const dia = 0;
+
+switch (dia) {
+  case 0:
+    console.log("Hoy es Domingo");
+    break;
+  case 1:
+    console.log("Hoy es Lunes");
+    break;
+  case 2:
+    console.log("Hoy es Martes");
+    break;
+  case 3:
+    console.log("Hoy es Miercoles");
+    break;
+  case 4:
+    console.log("Hoy es Jueves");
+    break;
+  case 5:
+    console.log("Hoy es Viernes");
+    break;
+  case 6:
+    console.log("Hoy es Sabado");
+    break;
+  default:
+    console.log("Dia no valido");
+    break;
+}
