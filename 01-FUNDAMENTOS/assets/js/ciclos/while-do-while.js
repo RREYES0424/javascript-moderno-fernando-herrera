154ec8ec3e00bec3b06d6994ff7c813aafd259ff
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-25 13:55:26 -0600

    ADDED: while-do-while.js

---
+++ 01-FUNDAMENTOS/assets/js/ciclos/while-do-while.js
@@ -0,0 +1,14 @@
+const carros = ['Ford','Honda','Mazda','Toyota'];
+let i = 0;
+while(i < carros.length){
+    console.log(carros[i]);
+    i++;
+}
+
+
+let j = 10;
+do{
+    console.log(carros[j]);
+    j++
+}while(carros[j]);
+
