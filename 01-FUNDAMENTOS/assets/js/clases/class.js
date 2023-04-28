79a27c081fddfe0a3e7078a28099e90f5134049a
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-28 11:55:35 -0600

    MODIFIED: class.js  - Conociendo set y get

--- 01-FUNDAMENTOS/assets/js/clases/class.js
+++ 01-FUNDAMENTOS/assets/js/clases/class.js
@@ -2,19 +2,34 @@ class Persona{
     nombre = '';
     edad = 0;
     pais = '';
+    comida = '';
+
     constructor(nombre = 'Sin Nombre',edad = 'Sin edad',pais = 'Sin pais'){
         this.nombre = nombre,
         this.edad = edad,
         this.pais = pais
     }
+
+    set setComidaFavorita( comida ){        
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita(){
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
+    }
+
     presentar(){
         console.log(`Hola! mi nombre es ${this.nombre}, tengo ${this.edad} años y soy de ${this.pais}`);
     }
 }
 
 const ricardo = new Persona('Ricardo Reyes Flores',23,'Mexico');
-console.log(ricardo);
 ricardo.presentar();
 
 const ironman = new Persona('Tony Stark',35,'California');
 ironman.presentar();
+
+
+ricardo.setComidaFavorita = 'Mole';
+ricardo.sexo = 'Masculino';
+console.log({ricardo});
